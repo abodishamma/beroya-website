30a0e44a17e7c50cbfbfb9c158dc7d7d3a8742d0
abodishamma <abodishamma@gmail.com>
2026-07-04 23:51:14 +0300

    Configure GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import process from "node:process";
 
 export default defineConfig({
+  base: process.env.GITHUB_PAGES === "true" ? "/beroya-website/" : "/",
   plugins: [react(), tailwindcss()],
   build: {
     target: "es2022",
